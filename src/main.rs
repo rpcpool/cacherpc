a7428be890b28d34cfa59640c073e274704e3128
Linus Kendall <me@linuskendall.com>
2021-09-14 08:54:13 +0100

    Added a configurable request timeout
    Added a configurable request timeout in order to debug issues related to zubr-exchange/cacherpc#48.

--- src/main.rs
+++ src/main.rs
@@ -18,6 +18,92 @@ use tokio::sync::{watch, Notify, Semaphore};
 use tracing::info;
 
 use mlua::{Lua, LuaOptions, StdLib};
+pub use cache_rpc::{accounts, metrics, rpc, types};
+
+use accounts::PubSubManager;
+use types::{AccountsDb, ProgramAccountsDb};
+
+#[derive(Debug, structopt::StructOpt)]
+#[structopt(about = "Solana RPC cache server")]
+struct Options {
+    #[structopt(
+        short = "w",
+        long = "websocket-url",
+        default_value = "wss://solana-api.projectserum.com",
+        help = "validator or cluster PubSub endpoint"
+    )]
+    ws_url: String,
+    #[structopt(
+        short = "r",
+        long = "rpc-api-url",
+        default_value = "https://solana-api.projectserum.com",
+        help = "validator or cluster JSON-RPC endpoint"
+    )]
+    rpc_url: String,
+    #[structopt(
+        short = "l",
+        long = "listen",
+        default_value = "127.0.0.1:8080",
+        help = "cache server bind address"
+    )]
+    addr: String,
+    #[structopt(
+        short = "p",
+        long = "program-request-limit",
+        default_value = "5",
+        help = "maximum number of concurrent getProgramAccounts cache-to-validator requests"
+    )]
+    program_accounts_request_limit: usize,
+    #[structopt(
+        short = "a",
+        long = "account-request-limit",
+        default_value = "100",
+        help = "maximum number of concurrent getAccountInfo cache-to-validator requests"
+    )]
+    account_info_request_limit: usize,
+    #[structopt(
+        short = "b",
+        long = "body-cache-size",
+        default_value = "100",
+        help = "maximum amount of entries in the response cache"
+    )]
+    body_cache_size: usize,
+    #[structopt(
+        long = "log-format",
+        default_value = "plain",
+        help = "one of: 'plain', 'json'"
+    )]
+    log_format: LogFormat,
+    #[structopt(
+        long = "request-timeout",
+        default_value = "30",
+        help = "request timeout in seconds"
+    )]
+    rpc_timeout: u64,
+    #[structopt(long = "log-file", help = "file path")]
+    log_file: Option<std::path::PathBuf>,
+    #[structopt(
+        short = "c",
+        long = "websocket-connections",
+        help = "number of WebSocket connections to validator",
+        default_value = "1"
+    )]
+    websocket_connections: u32,
+    #[structopt(
+        short = "t",
+        long = "time-to-live",
+        help = "time to live for cached values",
+        default_value = "10m",
+        parse(try_from_str = humantime::parse_duration)
+    )]
+    time_to_live: Duration,
+}
+
+#[derive(Debug)]
+enum LogFormat {
+    Plain,
+    Json,
+}
 
 pub use cache_rpc::{cli, metrics, pubsub, rpc, types};
 
@@ -156,6 +242,7 @@ async fn run(options: cli::Options) -> Result<()> {
         .transpose()?;
 
     let rpc_url = options.rpc_url;
+    let rpc_timeout = Duration::from_secs(options.rpc_timeout);
     let notify = Arc::new(Notify::new());
     let account_info_request_limit =
         Arc::new(Semaphore::new(config.rpc.request_limits.account_info));
@@ -204,6 +291,7 @@ async fn run(options: cli::Options) -> Result<()> {
             client,
             pubsub: pubsub.clone(),
             rpc_url: rpc_url.clone(),
+            rpc_timeout: rpc_timeout.clone(),
             map_updated: notify.clone(),
             account_info_request_limit: account_info_request_limit.clone(),
             program_accounts_request_limit: program_accounts_request_limit.clone(),
